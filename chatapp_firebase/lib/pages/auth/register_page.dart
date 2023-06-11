33dd2a0c4ca32e3f0959f744b3833ece5c49bc6f
203722 <203722@ids.upchiapas.edu.mx>
2023-06-11 12:27:15 -0600

    First commit

---
+++ chatapp_firebase/lib/pages/auth/register_page.dart
@@ -0,0 +1,221 @@
+import 'package:chatapp_firebase/helper/helper_function.dart';
+import 'package:chatapp_firebase/pages/auth/login_page.dart';
+import 'package:chatapp_firebase/pages/home_page.dart';
+import 'package:chatapp_firebase/service/auth_service.dart';
+import 'package:chatapp_firebase/widgets/widgets.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  List<String> listaDeOpciones = ["TOP","JG","MID","ADC","SUPP","CUALQUIERA"];
+  bool _isLoading = false;
+  final formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  String fullName = "";
+  String linea = "";
+  AuthService authService = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                  color: Theme.of(context).primaryColor))
+          : SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+                child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        const Text(
+                          "Registrate",
+                          style: TextStyle(
+                              fontSize: 60,
+                              //fontWeight: FontWeight.bold,
+                              fontFamily: "gideon roman",
+                              color: Color(0xFFFFB526)),
+                        ),
+                        const SizedBox(height: 100,),
+                        TextFormField(
+                          style: TextStyle(color: const Color.fromARGB(255, 189, 189, 189)),
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Full Name",
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Theme.of(context).primaryColor,
+                              )),
+                          onChanged: (val) {
+                            setState(() {
+                              fullName = val;
+                            });
+                          },
+                          validator: (val) {
+                            if (val!.isNotEmpty) {
+                              return null;
+                            } else {
+                              return "Name cannot be empty";
+                            }
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: const Color.fromARGB(255, 189, 189, 189)),
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Email",
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color: Theme.of(context).primaryColor,
+                              )),
+                          onChanged: (val) {
+                            setState(() {
+                              email = val;
+                            });
+                          },
+
+                          // check tha validation
+                          validator: (val) {
+                            return RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(val!)
+                                ? null
+                                : "Please enter a valid email";
+                          },
+                        ),
+                        const SizedBox(height: 15),
+                        TextFormField(
+                          style: TextStyle(color: const Color.fromARGB(255, 189, 189, 189)),
+                          obscureText: true,
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Password",
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Theme.of(context).primaryColor,
+                              )),
+                          validator: (val) {
+                            if (val!.length < 6) {
+                              return "Password must be at least 6 characters";
+                            } else {
+                              return null;
+                            }
+                          },
+                          onChanged: (val) {
+                            setState(() {
+                              password = val;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 15),
+                        DropdownButtonFormField<String>(
+                          items: listaDeOpciones.map((e) {
+                            return DropdownMenuItem<String>(
+                              value: e,
+                              child: Text(
+                                e,
+                                style: TextStyle(
+                                  color: const Color.fromARGB(255, 189, 189, 189),
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              linea = value!;
+                            });
+                          },
+                          isDense: true,
+                          isExpanded: true,
+                          decoration: textInputDecoration.copyWith(
+                            labelText: "Linea Preferida",
+                            prefixIcon: Icon(
+                              Icons.arrow_drop_down,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Theme.of(context).primaryColor,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30))),
+                            child: const Text(
+                              "Register",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            onPressed: () {
+                              register();
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text.rich(TextSpan(
+                          text: "Already have an account? ",
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255), fontSize: 14),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: "Login now",
+                                style: const TextStyle(
+                                    color: Color.fromARGB(255, 255, 40, 40),
+                                    decoration: TextDecoration.underline),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    nextScreen(context, const LoginPage());
+                                  }),
+                          ],
+                        )),
+                      ],
+                    )),
+              ),
+            ),
+    );
+  }
+
+  register() async {
+    if (formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+      await authService
+          .registerUserWithEmailandPassword(fullName, email, password, linea)
+          .then((value) async {
+        if (value == true) {
+          // saving the shared preference state
+          await HelperFunctions.saveUserLoggedInStatus(true);
+          await HelperFunctions.saveUserEmailSF(email);
+          await HelperFunctions.saveUserNameSF(fullName);
+          nextScreenReplace(context, const HomePage());
+        } else {
+          showSnackbar(context, Color(0xFFFFB526), value);
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      });
+    }
+  }
+}
